f1bedf0671103a89a85691607d14222989c51805
Brad Dettmer <11279144+redSlug@users.noreply.github.com>
2020-01-07 23:15:59 -0500

    update

--- sleep.py
+++ sleep.py
@@ -11,59 +11,98 @@ HEADER = {'Authorization': f"Bearer {ACCESS_TOKEN}"}
 FB_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
 READ_FORMAT = "%Y-%m-%d %H:%M"
 SHORT_FORMAT = "%Y-%m-%d"
-EMPTY = ['', '', '', '', '', '', '', '']
 
 
-def get_sleep_data(date):
-    url = f"{BASE_URL}/1.2/user/{USER_NAME}/sleep/date/{date}.json?date{date}=&id={USER_NAME}"
-    r = requests.get(url, headers=HEADER)
-    content = json.loads(r.content)
+def _get_date_time(data):
+    return datetime.strptime(data['dateTime'], FB_FORMAT)
 
-    if len(content['sleep']) != 1:
-        print(f"skipping {date}, wrong content length")
-        return EMPTY
 
-    sleep_data = content['sleep'][0]
-    sleep_summary = content['summary']
+def _awake_before(data, time):
+    return data['level'] == 'wake' and _get_date_time(data) < time
 
-    minutes_asleep = sleep_data['minutesAsleep']
-    minutes_in_bed = sleep_data['minutesAwake'] + minutes_asleep
 
+def awake_minutes_minus(level_data, hours=1):
+    wake_time = _get_date_time(level_data[-1])
+    last_hour_start = wake_time - timedelta(hours=hours)
+    seconds_awake = [l['seconds'] for l in level_data if _awake_before(l, last_hour_start)]
+    return round(sum(seconds_awake) / 60)
+
+
+def process_sleep(data):
+    sleep = data.get('sleep')
+    summary = data.get('summary')
+
+    if len(sleep) != 1 or 'stages' not in summary:
+        return None
+
+    sleep = sleep[0]
+    stages = summary['stages']
+
+    minutes_deep = stages['deep']
+    minutes_rem = stages['rem']
+    minutes_light = stages['light']
+    minutes_wake = stages['wake']
+
+    if not(minutes_deep or minutes_rem or minutes_light or minutes_wake):
+        return None
+
+    minutes_asleep = sleep['minutesAsleep']
+    minutes_in_bed = sleep['minutesAwake'] + minutes_asleep
     sleep_ratio = round(minutes_asleep / minutes_in_bed, 2)
-    if 'stages' not in sleep_summary:
-        print(f"skipping {date}, stages not in summary")
-        return EMPTY
 
-    minutes_deep = sleep_summary['stages']['deep']
-    minutes_rem = sleep_summary['stages']['rem']
-    minutes_light = sleep_summary['stages']['light']
-    minutes_wake = sleep_summary['stages']['wake']
+    level_data = sleep['levels']['data']
 
-    level_data = sleep_data['levels']['data']
-
-    if 'levels' not in sleep_data or 'data' not in sleep_data['levels']:
-        print(f"skipping {date}, levels data not provided")
-        return EMPTY
+    minutes_awake_1 = awake_minutes_minus(level_data, hours=1)
+    minutes_awake_2 = awake_minutes_minus(level_data, hours=2)
 
     sleep_time = datetime.strptime(level_data[0]['dateTime'], FB_FORMAT).strftime(READ_FORMAT)
     wake_time = datetime.strptime(level_data[-1]['dateTime'], FB_FORMAT).strftime(READ_FORMAT)
+    efficiency = sleep['efficiency']
 
-    return [sleep_time, wake_time, sleep_ratio, minutes_asleep, minutes_deep, minutes_rem, minutes_light, minutes_wake]
+    return [sleep_time, wake_time, sleep_ratio, minutes_asleep, minutes_deep, minutes_rem, minutes_light, minutes_wake, efficiency, minutes_awake_1, minutes_awake_2]
+
+
+def get_header():
+    return ["sleep_time", "wake_time", "sleep_ratio", "minutes_asleep", "minutes_deep", "minutes_rem", "minutes_light", "minutes_wake", "efficiency", "minutes_awake_1", "minutes_awake_2"]
+
+
+def get_api_data(date):
+    url = f"{BASE_URL}/1.2/user/{USER_NAME}/sleep/date/{date}.json?date{date}=&id={USER_NAME}"
+    r = requests.get(url, headers=HEADER)
+    return json.loads(r.content)
+
+
+def write_sleep_data():
+    date = datetime(2019, 11, 1)
+    yesterday = datetime.utcnow() - timedelta(days=1)
+    lines = list()
+    while date < yesterday:
+        sleep_data = get_api_data(date.strftime(SHORT_FORMAT))
+        lines.append(sleep_data)
+        date += timedelta(days=1)
+    with open('sleep.json', mode='w') as sleep_file:
+        sleep_file.write(json.dumps(lines))
+    return True
+
+
+def read_sleep_data():
+    with open('sleep.json', mode='r') as sleep_file:
+        sleep_json = sleep_file.readline()
+        return json.loads(sleep_json)
+
+
+def write_sleep_csv(data):
+    with open('sleep.csv', mode='w') as sleep_data:
+        writer = csv.writer(sleep_data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(get_header())
+
+        for item in data:
+            result = process_sleep(item)
+            if result:
+                writer.writerow(result)
 
 
 if __name__ == "__main__":
-
-    with open('sleep_data.csv', mode='w') as sleep_data:
-        writer = csv.writer(sleep_data, delimiter=',', quotechar='"',
-                                     quoting=csv.QUOTE_MINIMAL)
-
-        date = datetime(2019, 11, 1)
-        three_days_ago = datetime.utcnow() - timedelta(days=3)
-
-        writer.writerow(
-            ['sleep', 'wake', 'ratio', 'minutes_asleep', 'minutes_deep', 'miniutes_rem', 'minutes_light', 'minutes_wake']
-        )
-
-        while date < three_days_ago:
-            date += timedelta(days=1)
-            writer.writerow(get_sleep_data(date.strftime(SHORT_FORMAT)))
+    # write_sleep_data()
+    data = read_sleep_data()
+    write_sleep_csv(data)
